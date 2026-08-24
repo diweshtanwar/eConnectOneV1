16dbe43f572184d13403c16cd8b227be81dc8909
Diwesh Tanwar <diwesh.tanwar@gmail.com>
2026-08-24 14:33:09 +0530

    add SyncModelChanges migration

--- backend/eConnectOne.API/Migrations/20260824090250_SyncModelChanges.Designer.cs
+++ backend/eConnectOne.API/Migrations/20260824090250_SyncModelChanges.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using eConnectOne.API.Data;
@@ -11,9 +12,11 @@ using eConnectOne.API.Data;
 namespace eConnectOne.API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260824090250_SyncModelChanges")]
+    partial class SyncModelChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
